92ecfe4482fbc6ae2b0960903823e7d4cabfa0a0
membrowse <michael@membrowse.com>
2026-01-27 19:24:20 +0000

    Deploying to gh-pages from @ membrowse/cmrx@33f43b4bd36f44731250993d69ba313ed88b608e 🚀

--- navtreedata.js
+++ navtreedata.js
@@ -48,20 +48,20 @@ var NAVTREE =
     [ "Getting started", "getting_started.html", [
       [ "HOWTO: Integrating CMRX into CubeMX project", "getting_started_cubemx.html", [
         [ "Prerequisites", "getting_started_cubemx.html#autotoc_md37", null ],
-        [ "Creating project skeleton with CubeMX", "getting_started_cubemx.html#autotoc_md41", null ],
+        [ "Creating project skeleton with CubeMX", "getting_started_cubemx.html#autotoc_md42", null ],
         [ "Basic CubeMX project settings", "getting_started_cubemx.html#autotoc_md44", null ],
         [ "Configuring the core", "getting_started_cubemx.html#autotoc_md46", null ],
         [ "Configuring GPIOs", "getting_started_cubemx.html#autotoc_md48", null ],
-        [ "Generating project and getting CMRX source", "getting_started_cubemx.html#autotoc_md49", null ],
-        [ "Integrating CMRX into CubeMX project", "getting_started_cubemx.html#autotoc_md51", [
+        [ "Generating project and getting CMRX source", "getting_started_cubemx.html#autotoc_md50", null ],
+        [ "Integrating CMRX into CubeMX project", "getting_started_cubemx.html#autotoc_md52", [
           [ "1. Add CMake modules provided by CMRX into CMake module path", "getting_started_cubemx.html#autotoc_md53", null ],
           [ "2. Configure CubeMX integration", "getting_started_cubemx.html#autotoc_md54", null ],
-          [ "3. Configure the CMRX kernel to use CubeMX", "getting_started_cubemx.html#autotoc_md56", null ]
+          [ "3. Configure the CMRX kernel to use CubeMX", "getting_started_cubemx.html#autotoc_md57", null ]
         ] ],
         [ "Linking CMRX to the project", "getting_started_cubemx.html#autotoc_md58", null ],
-        [ "Creating the blinky application", "getting_started_cubemx.html#autotoc_md61", null ],
-        [ "CubeMX HAL tweaks", "getting_started_cubemx.html#autotoc_md64", null ],
-        [ "Adding blinky application to the build", "getting_started_cubemx.html#autotoc_md67", null ],
+        [ "Creating the blinky application", "getting_started_cubemx.html#autotoc_md62", null ],
+        [ "CubeMX HAL tweaks", "getting_started_cubemx.html#autotoc_md66", null ],
+        [ "Adding blinky application to the build", "getting_started_cubemx.html#autotoc_md68", null ],
         [ "Flashing the application", "getting_started_cubemx.html#autotoc_md69", null ]
       ] ],
       [ "HOWTO: Integrating CMRX into Pico-SDK project", "getting_started_picosdk.html", [
--- search/all_7.js
+++ search/all_7.js
@@ -16,7 +16,7 @@ var searchData=
   ['device_13',['device',['../structEmuDummyPeripheral__t.html#a90ad79b58b95e238446c2683086be8fc',1,'EmuDummyPeripheral_t::device'],['../findcmsis.html#autotoc_md23',1,'DEVICE']]],
   ['device_5finclude_5ffilename_14',['DEVICE_INCLUDE_FILENAME',['../findcmsis.html#autotoc_md25',1,'']]],
   ['diff_5fmicrotime_15',['diff_microtime',['../group__api__standard.html#ga821cccfa3f3ac511dd361a3544715944',1,'std.c']]],
-  ['directories_16',['Source and header directories',['../dev_env.html#autotoc_md65',1,'']]],
+  ['directories_16',['Source and header directories',['../dev_env.html#autotoc_md64',1,'']]],
   ['dispatch_5fargs_17',['dispatch_args',['../structsyscall__dispatch__t.html#a38c4c9617d66066d5386da2ce3a513a9',1,'syscall_dispatch_t']]],
   ['dispatch_5ftarget_18',['dispatch_target',['../structsyscall__dispatch__t.html#ab5684536818cdbc428d1689b3195a52c',1,'syscall_dispatch_t']]],
   ['do_5fset_5ftimed_5fevent_19',['do_set_timed_event',['../group__os__timer.html#gaef90c3e49cedd75a22cf512bad8c149f',1,'timer.c']]],
